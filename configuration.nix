71657840d02902ea61088590c8f2a3f59566373a
Benjamin Mellor <ben@smokingkangaroo.com>
2015-12-22 10:49:51 +1100

    don't install vim in system profile

--- configuration.nix
+++ configuration.nix
@@ -33,7 +33,6 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    vim
     wget
     zsh
   ];
