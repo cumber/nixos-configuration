97eaaf72181b98fca233170ce9ce4e2cf747a20d
Benjamin Mellor <ben@smokingkangaroo.com>
2016-10-06 14:09:30 +1100

    update gummiboot -> systemd-boot for nixos-16.09

--- .nixpkgs/nixos/vanwa.nix
+++ .nixpkgs/nixos/vanwa.nix
@@ -2,8 +2,8 @@
   {
     networking.hostName = "vanwa";
 
-    # Use the gummiboot efi boot loader.
-    boot.loader.gummiboot.enable = true;
+    # Use the systemd-boot efi boot loader.
+    boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
     services.xserver.synaptics.enable = true;
