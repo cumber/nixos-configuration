a34a8edeb0a760c0e6207b192f8772353a0f315f
Benjamin Mellor <ben@smokingkangaroo.com>
2016-11-07 13:36:20 +1100

    use alyamar as a binary cache

--- .nixpkgs/nixos/vanwa.nix
+++ .nixpkgs/nixos/vanwa.nix
@@ -6,5 +6,16 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
+    nix = {
+      binaryCaches = [
+        "http://alyamar.goldie:5000"
+        "https://cache.nixos.org/"
+      ];
+      binaryCachePublicKeys = [
+        "alyamar:6Gg2JIhXz/NTUI77hdIlTkNlArdF9yvz3dqNRhpjXuo="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+    };
+
     services.xserver.synaptics.enable = true;
   }
