b9e7e1d0cdb3835ca4b798e473fe7517f0d09d7c
Benjamin Mellor <ben@smokingkangaroo.com>
2017-11-09 10:49:22 +1100

    add cumber's user shell to /etc/shells
    (probably should revisit this shell configuration entirely)

--- .nixpkgs/nixos/configuration.nix
+++ .nixpkgs/nixos/configuration.nix
@@ -97,6 +97,10 @@
     };
   };
 
+  environment.shells = [
+    "/home/cumber/.nix-profile/bin/zsh"
+  ];
+
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "16.03";
 
