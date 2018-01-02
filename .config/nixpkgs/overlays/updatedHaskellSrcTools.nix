38954dfaebbc5fcec7cfc6dcba160616f20fd59e
Benjamin Mellor <ben@smokingkangaroo.com>
2018-01-02 20:54:53 +1100

    updated haskell-src-exts is now 1.20.1

--- .config/nixpkgs/overlays/updatedHaskellSrcTools.nix
+++ .config/nixpkgs/overlays/updatedHaskellSrcTools.nix
@@ -1,7 +1,7 @@
 self: super: {
   updatedHaskellSrcTools = (
     let hpkgs = self.haskellPackages.extend (hpkgsSelf: hpkgsSuper: {
-          haskell-src-exts = hpkgsSelf.haskell-src-exts_1_19_1;
+          haskell-src-exts = hpkgsSelf.haskell-src-exts_1_20_1;
         });
      in { inherit (hpkgs) hlint; }
   );
