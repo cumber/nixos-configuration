9f2465bf2ff4046b1f4e05313fa987cd50903df1
Benjamin Mellor <ben@smokingkangaroo.com>
2018-04-04 11:39:46 +0100

    temporarily stop using alyamar as binary cache while travelling

--- .config/nixpkgs/overlays/nixos-config/nixos/vanwa.nix
+++ .config/nixpkgs/overlays/nixos-config/nixos/vanwa.nix
@@ -8,11 +8,11 @@
 
     nix = {
       binaryCaches = [
-        "http://alyamar.goldie:5000"
+        # "http://alyamar.goldie:5000"
         "https://cache.nixos.org/"
       ];
       binaryCachePublicKeys = [
-        "alyamar:6Gg2JIhXz/NTUI77hdIlTkNlArdF9yvz3dqNRhpjXuo="
+        # "alyamar:6Gg2JIhXz/NTUI77hdIlTkNlArdF9yvz3dqNRhpjXuo="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
     };
