3d7327c10125d13160742885ddb1c274fc29f619
Benjamin Mellor <ben@smokingkangaroo.com>
2018-06-22 12:40:10 +1000

    override version of status-notifier-item to improve tray icon behaviour

--- .config/nixpkgs/overlays/taffybar-2-fix.nix
+++ .config/nixpkgs/overlays/taffybar-2-fix.nix
@@ -12,6 +12,13 @@ self: super: {
       };
     });
 
+    status-notifier-item = hpSelf.callCabal2nix "status-notifier-item" (super.fetchFromGitHub {
+      owner = "IvanMalison";
+      repo = "status-notifier-item";
+      rev = "bb7ed5c07418484bda3a9c1cbc513830a83fd158";
+      sha256 = "1a5z6xfz9cfjswgvxdr24wcxqbg4g0rivqds38zx3mzg520zmwws";
+    }) {};
+
     xmonad-contrib = hpSelf.callCabal2nix "xmonad-contrib" (super.fetchFromGitHub {
       owner = "xmonad";
       repo = "xmonad-contrib";
