b2ee89e0b71d11597800cca536cce790be368da7
Benjamin Mellor <ben@smokingkangaroo.com>
2018-06-25 11:18:04 +1000

    don't need to override so much now taffybar 2.1.2 is in nixos unstable

--- .config/nixpkgs/overlays/taffybar-2-fix.nix
+++ .config/nixpkgs/overlays/taffybar-2-fix.nix
@@ -1,17 +1,5 @@
 self: super: {
-  haskellPackages = super.haskellPackages.extend (hpSelf: hpSuper:
-    {
-    taffybar = hpSuper.taffybar.overrideDerivation (drv: {
-      name = "taffybar-2.1.2";
-      version = "2.1.2";
-      src = super.fetchFromGitHub {
-        owner = "taffybar";
-        repo = "taffybar";
-        rev = "v2.1.2";
-        sha256 = "0snjwx75qf9ccf4j99cdsh0njh4pzl8s4lmarrfs6l0mirgh4grw";
-      };
-    });
-
+  haskellPackages = super.haskellPackages.extend (hpSelf: hpSuper: {
     status-notifier-item = hpSelf.callCabal2nix "status-notifier-item" (super.fetchFromGitHub {
       owner = "IvanMalison";
       repo = "status-notifier-item";
@@ -25,30 +13,5 @@ self: super: {
       rev = "13e37b964e82f2fcd790530edcdeb8f7cb801121";
       sha256 = "1dbb711m3z7i05klyzgx77z039hsg8pckfb3ar17ffc2yzc2mnad";
     }) {};
-
-    "gi-dbusmenugtk3" = hpSelf.callPackage
-    ({ mkDerivation, base, bytestring, Cabal, containers, gi-atk
-     , gi-dbusmenu, gi-gdk, gi-gdkpixbuf, gi-glib, gi-gobject, gi-gtk
-     , haskell-gi, haskell-gi-base, haskell-gi-overloading
-     , libdbusmenu-gtk3, text, transformers, system-gtk3
-     }:
-     mkDerivation {
-       pname = "gi-dbusmenugtk3";
-       version = "0.4.1";
-       sha256 = "0gl37jsska2qsakzbmvwvb33lskdrbxpk1hmw907y187d0hq7pry";
-       setupHaskellDepends = [ base Cabal haskell-gi ];
-       libraryHaskellDepends = [
-         base bytestring containers gi-atk gi-dbusmenu gi-gdk gi-gdkpixbuf
-         gi-glib gi-gobject gi-gtk haskell-gi haskell-gi-base
-         haskell-gi-overloading text transformers
-       ];
-       libraryPkgconfigDepends = [ libdbusmenu-gtk3 system-gtk3.dev];
-       doHaddock = false;
-       homepage = "https://github.com/haskell-gi/haskell-gi";
-       description = "DbusmenuGtk bindings";
-       license = self.stdenv.lib.licenses.lgpl21;
-       hydraPlatforms = self.stdenv.lib.platforms.none;
-     }) {inherit (self) libdbusmenu-gtk3; system-gtk3 = self.gtk3;};
-    }
-  );
+  });
 }
