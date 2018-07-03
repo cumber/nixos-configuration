f26e3f5352ada46c6e55441e2a09b64e6c0c2d2b
Benjamin Mellor <ben@smokingkangaroo.com>
2018-07-03 18:06:45 +1000

    unreleased status-notifier-item that makes slack and signal icon updates work

--- .config/nixpkgs/overlays/taffybar-2-fix.nix
+++ .config/nixpkgs/overlays/taffybar-2-fix.nix
@@ -1,5 +1,12 @@
 self: super: {
   haskellPackages = super.haskellPackages.extend (hpSelf: hpSuper: {
+    status-notifier-item = hpSelf.callCabal2nix "status-notifier-item" (super.fetchFromGitHub {
+      owner = "IvanMalison";
+      repo = "status-notifier-item";
+      rev = "e2cfd82133d02f0df959f1bb0316b9e58ec453d6";
+      sha256 = "0v37pfnn7m02jbfc68qa7snncilfad8iyvqcjq85q148qrl53zxm";
+    }) {};
+
     xmonad-contrib = hpSelf.callCabal2nix "xmonad-contrib" (super.fetchFromGitHub {
       owner = "xmonad";
       repo = "xmonad-contrib";
