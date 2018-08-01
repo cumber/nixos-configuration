0d5168ac176a5ae041a19cf44d63fcada550ea09
Benjamin Mellor <ben@smokingkangaroo.com>
2018-08-01 12:04:02 +1000

    use web-mode for .hbs template files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -170,6 +170,7 @@
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
 
 (use-package tern
