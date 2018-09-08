83a05bc09be6540ef2a8f56d1a36becb028a6a45
Benjamin Mellor <ben@smokingkangaroo.com>
2018-09-08 12:20:16 +1000

    xmonad-contrib fullscreen support pull request
    Previously was using a patch suggested in the bug report; this is one
    step closer to being "official".

--- .config/nixpkgs/overlays/xmonad-contrib-fullscreen-test/default.nix
+++
@@ -1,7 +0,0 @@
-self: super: {
-  haskellPackages = super.haskellPackages.extend (hself: hsuper: {
-    xmonad-contrib = hsuper.xmonad-contrib.overrideAttrs (old: {
-      patches = (old.patches or []) ++ [ ./xmonad-contrib-0.14.patch ];
-    });
-  });
-}
