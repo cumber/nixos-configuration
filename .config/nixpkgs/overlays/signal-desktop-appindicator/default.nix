5096570c6b782d2859ada8963b78b5cca6274c8c
Benjamin Mellor <ben@smokingkangaroo.com>
2019-04-02 11:37:02 +1100

    use overlays rather than copy+hack for appindicator support in slack and signal

--- .config/nixpkgs/overlays/signal-desktop-appindicator/default.nix
+++ .config/nixpkgs/overlays/signal-desktop-appindicator/default.nix
@@ -1,3 +1,34 @@
 self: super: {
-  signal-desktop = super.callPackage ./signal-desktop.nix {};
+  signal-desktop = self.stdenv.mkDerivation (
+    let sd = super.signal-desktop;
+     in {
+          name = sd.name;
+          version = sd.version;
+          meta = sd.meta;
+
+          nativeBuildInputs = [ self.binutils ];
+          phases = [ "installPhase" ];
+          installPhase = ''
+            cp -r ${sd} $out
+
+            # Patch to include appindicator
+            chmod u+w $out/libexec/.signal-desktop-wrapped
+            runpath="$(readelf -d $out/libexec/.signal-desktop-wrapped | grep RUNPATH | sed -e 's/^.*\[\(.*\)]$/\1/' | sed -e "s|${sd}|$out|"):${self.libappindicator-gtk3}/lib"
+            patchelf --set-rpath $runpath $out/libexec/.signal-desktop-wrapped
+
+            # Fix previous wrapper script; absolute path to overridden package
+            substituteInPlace $out/libexec/signal-desktop \
+              --replace ${sd} $out
+
+            # Replace symlink in bin; absolute path to overridden package
+            chmod u+w $out/bin
+            rm $out/bin/signal-desktop
+            ln -s $out/libexec/signal-desktop $out/bin/signal-desktop
+
+            # Fix path in desktop link
+            substituteInPlace $out/share/applications/signal-desktop.desktop \
+              --replace ${sd} $out
+          '';
+        }
+  );
 }
--- .config/nixpkgs/overlays/slack-appindicator/default.nix
+++ .config/nixpkgs/overlays/slack-appindicator/default.nix
@@ -1,3 +1,30 @@
 self: super: {
-  slack = super.callPackage ./slack.nix {};
+  slack = self.stdenv.mkDerivation (
+    let sl = super.slack;
+     in {
+          name = sl.name;
+          meta = sl.meta;
+
+          nativeBuildInputs = [ self.binutils self.file ];
+          phases = [ "installPhase" ];
+          installPhase = ''
+            cp -r ${sl} $out
+
+            # Patch to include appindicator
+            for file in $(find $out -type f -exec file '{}' ';' | grep ELF | sed 's/^\(.*\):.*$/\1/'); do
+              runpath="$(readelf -d $file | grep RUNPATH | sed -e 's/^.*\[\(.*\)]$/\1/' | sed -e "s|${sl}|$out|"):${self.libappindicator-gtk3}/lib"
+              chmod u+w $file
+              patchelf --set-rpath $runpath $file
+            done
+
+            # Fix previous wrapper script; absolute path to overridden package
+            substituteInPlace $out/bin/slack \
+              --replace ${sl} $out
+
+            # Fix path in desktop link
+            substituteInPlace $out/share/applications/slack.desktop \
+              --replace ${sl} $out
+          '';
+        }
+  );
 }
