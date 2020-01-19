bee39006a83ccd960d9a28bee02f992912933874
Benjamin Mellor <ben@smokingkangaroo.com>
2020-01-19 17:23:58 +1300

    taffybar was marked broken upstream because of the problem with gi-gdkx11

--- .config/nixpkgs/overlays/gi-gdkx11_no-gtk4/default.nix
+++ .config/nixpkgs/overlays/gi-gdkx11_no-gtk4/default.nix
@@ -5,5 +5,8 @@
 self: super: {
   haskellPackages = super.haskellPackages.extend (hself: hsuper: {
     gi-gdkx11 = super.haskellPackages.callHackage "gi-gdkx11" "3.0.9" { gtk3 = self.gtk3; };
+
+    # taffybar was marked broken upstream, because of the dependency on gi-gdkx11
+    taffybar = hsuper.taffybar.overrideAttrs (old: { meta = old.meta // { broken = false; }; });
   });
 }
