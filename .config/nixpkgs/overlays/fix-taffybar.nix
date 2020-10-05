402b907f5f7448c50eae1ebcf50163573407e444
Benjamin Mellor <ben@smokingkangaroo.com>
2020-10-05 16:36:55 +1300

    unbreak taffybar again so I can finally update my profile

--- .config/nixpkgs/overlays/fix-taffybar.nix
+++ .config/nixpkgs/overlays/fix-taffybar.nix
@@ -1,32 +1,22 @@
 # https://github.com/NixOS/nixpkgs/issues/63500#issuecomment-674835273
+# updated for further developments as of https://github.com/NixOS/nixpkgs/issues/63500#issuecomment-692270010
 self: super:
 {
   haskellPackages = with self.haskell.lib; super.haskellPackages.extend (hself: hsuper: {
-    gi-cairo-render = overrideCabal (hsuper.gi-cairo-render) (drv: {
-      src = self.fetchFromGitHub {
-        owner = "thestr4ng3r";
-        repo = "gi-cairo-render";
-        rev = "8727c43cdf91aeedffc9cb4c5575f56660a86399";
-        sha256 = "16kqh2ck0dad1l4m6q9xs5jqj9q0vgpqrzb2dc90jk8xwslmmhxd";
-      };
-      editedCabalFile = null;
-      postUnpack = ''
-        mv source all
-        mv all/gi-cairo-render source
-      '';
-    });
-    gi-dbusmenu = hself.gi-dbusmenu_0_4_8;
-    gi-dbusmenugtk3 = hself.gi-dbusmenugtk3_0_4_9;
-    gi-gdk = hself.gi-gdk_3_0_23;
-    gi-gdkx11 = overrideSrc hsuper.gi-gdkx11 {
+    gi-cairo-render = hself.callHackage "gi-cairo-render" "0.1.0" { inherit (self) cairo; };
+    gi-cairo-connector = hself.callHackage "gi-cairo-connector" "0.1.0" {};
+    gi-dbusmenu = markUnbroken hself.gi-dbusmenu_0_4_8;
+    gi-dbusmenugtk3 = markUnbroken hself.gi-dbusmenugtk3_0_4_9;
+    gi-gdk = markUnbroken hself.gi-gdk_3_0_23;
+    gi-gdkx11 = markUnbroken (overrideSrc hsuper.gi-gdkx11 {
       src = self.fetchurl {
         url = "https://hackage.haskell.org/package/gi-gdkx11-3.0.10/gi-gdkx11-3.0.10.tar.gz";
         sha256 = "0kfn4l5jqhllz514zw5cxf7181ybb5c11r680nwhr99b97yy0q9f";
       };
       version = "3.0.10";
-    };
-    gi-gtk-hs = hself.gi-gtk-hs_0_3_9;
-    gi-xlib = hself.gi-xlib_2_0_9;
+    });
+    gi-gtk-hs = markUnbroken hself.gi-gtk-hs_0_3_9;
+    gi-xlib = markUnbroken hself.gi-xlib_2_0_9;
     gtk-sni-tray = markUnbroken (hsuper.gtk-sni-tray);
     gtk-strut = markUnbroken (hsuper.gtk-strut);
     taffybar = markUnbroken (appendPatch hsuper.taffybar (self.fetchpatch {
