f0e291e29b710b9ec8db5003f10bee0985d8f6f4
Benjamin Mellor <ben@smokingkangaroo.com>
2020-12-16 14:32:03 +1300

    update taffybar overrides and base constraints for upstream changes

--- packages/desktop/fix-taffybar.nix
+++ packages/desktop/fix-taffybar.nix
@@ -1,27 +1,10 @@
 # https://github.com/NixOS/nixpkgs/issues/63500#issuecomment-674835273
 # updated for further developments as of https://github.com/NixOS/nixpkgs/issues/63500#issuecomment-692270010
+# Deleted most of this; upstream seems to have incorporated the patches and switched to the working
+# versions by detault, the only remaining issue is that taffybar is still marked as broken!
 self: super:
 {
   haskellPackages = with self.haskell.lib; super.haskellPackages.extend (hself: hsuper: {
-    gi-cairo-render = hself.callHackage "gi-cairo-render" "0.1.0" { inherit (self) cairo; };
-    gi-cairo-connector = hself.callHackage "gi-cairo-connector" "0.1.0" {};
-    gi-dbusmenu = markUnbroken hself.gi-dbusmenu_0_4_8;
-    gi-dbusmenugtk3 = markUnbroken hself.gi-dbusmenugtk3_0_4_9;
-    gi-gdk = markUnbroken hself.gi-gdk_3_0_23;
-    gi-gdkx11 = markUnbroken (overrideSrc hsuper.gi-gdkx11 {
-      src = self.fetchurl {
-        url = "https://hackage.haskell.org/package/gi-gdkx11-3.0.10/gi-gdkx11-3.0.10.tar.gz";
-        sha256 = "0kfn4l5jqhllz514zw5cxf7181ybb5c11r680nwhr99b97yy0q9f";
-      };
-      version = "3.0.10";
-    });
-    gi-gtk-hs = markUnbroken hself.gi-gtk-hs_0_3_9;
-    gi-xlib = markUnbroken hself.gi-xlib_2_0_9;
-    gtk-sni-tray = markUnbroken (hsuper.gtk-sni-tray);
-    gtk-strut = markUnbroken (hsuper.gtk-strut);
-    taffybar = markUnbroken (appendPatch hsuper.taffybar (self.fetchpatch {
-      url = "https://github.com/taffybar/taffybar/pull/494/commits/a7443324a549617f04d49c6dfeaf53f945dc2b98.patch";
-      sha256 = "0prskimfpapgncwc8si51lf0zxkkdghn33y3ysjky9a82dsbhcqi";
-    }));
+    taffybar = markUnbroken hsuper.taffybar;
   });
 }
