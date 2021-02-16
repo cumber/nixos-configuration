929de725a43ce5597a92ec7ab0114a162fb63957
Benjamin Mellor <ben@smokingkangaroo.com>
2021-02-16 16:39:06 +1300

    regen xmonad-custom nix file
    1. Was getting warnings about referencing stdenv.lib instead of just
       lib; cabal2nix has been updated in the meantime to take care of that.
    2. No longer use synapse, but was still pulling it in as a dependency
       because Ι forgot to regenerate the nix file.

--- packages/desktop/xmonad-custom/xmonad-custom.nix
+++ packages/desktop/xmonad-custom/xmonad-custom.nix
@@ -1,6 +1,6 @@
-{ mkDerivation, base, gdk_pixbuf, gtk3, lightlocker
-, pulseaudioLight, stdenv, synapse, taffybar, unix, wrapGAppsHook
-, X11, xmonad, xmonad-contrib
+{ mkDerivation, base, gdk_pixbuf, gtk3, lib, lightlocker
+, pulseaudioLight, taffybar, unix, wrapGAppsHook, X11, xmonad
+, xmonad-contrib
 }:
 mkDerivation {
   pname = "xmonad-custom";
@@ -12,7 +12,7 @@ mkDerivation {
     base taffybar unix X11 xmonad xmonad-contrib
   ];
   description = "My XMonad build";
-  license = stdenv.lib.licenses.bsd3;
+  license = lib.licenses.bsd3;
   executableSystemDepends = [
     wrapGAppsHook
     gtk3
@@ -23,13 +23,11 @@ mkDerivation {
       --replace '{{out}}' "$out" \
       --replace '{{lightlocker}}' '${lightlocker}' \
       --replace '{{pulseaudioLight}}' '${pulseaudioLight}' \
-      --replace '{{synapse}}' '${synapse}' \
     ;
     substituteInPlace src/Xmonad.hs \
       --replace '{{out}}' "$out" \
       --replace '{{lightlocker}}' '${lightlocker}' \
       --replace '{{pulseaudioLight}}' '${pulseaudioLight}' \
-      --replace '{{synapse}}' '${synapse}' \
     ;
   '';
 }
