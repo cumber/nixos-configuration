7be8cee070f585e630f56474cea773165de5e39c
Benjamin Mellor <ben@smokingkangaroo.com>
2022-02-09 13:39:52 +1300

    nix binary cache settings have been renamed

--- system/modules/haskell-nix.nix
+++ system/modules/haskell-nix.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
   {
-    nix = {
-      binaryCaches = [
+    nix.settings = {
+      substituters = [
         "https://hydra.iohk.io"
       ];
 
-      binaryCachePublicKeys = [
+      trusted-public-keys = [
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
     };
--- system/modules/reflex-frp.nix
+++ system/modules/reflex-frp.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
   {
-    nix = {
-      binaryCaches = [
+    nix.settings = {
+      substituters = [
         "https://nixcache.reflex-frp.org"
       ];
 
-      binaryCachePublicKeys = [
+      trusted-public-keys = [
         "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
       ];
     };
