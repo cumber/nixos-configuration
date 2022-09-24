208e51885e97d21bb06d2b652cb78a84cdefd766
Benjamin Mellor <ben@smokingkangaroo.com>
2022-09-24 23:36:00 +1000

    cleanup after emacs config move
    Mainly mass-indentation changes I didn't want to make earlier so that
    git would recognise the files as moved, not new.

--- users/modules/emacs/default.nix
+++ users/modules/emacs/default.nix
@@ -14,119 +14,119 @@ in
 
     extraConfig = builtins.readFile emacs-config;
     extraPackages = (epkgs: with epkgs; [
-    # foundation of package config in emacsd/init.el
-    use-package
+      # foundation of package config in emacsd/init.el
+      use-package
 
-    ###
-    # General editor behaviour
-    ###
+      ###
+      # General editor behaviour
+      ###
 
-    # code checking in editor
-    flycheck
+      # code checking in editor
+      flycheck
 
-    # company (code-completion) front end with icons
-    company-box
+      # company (code-completion) front end with icons
+      company-box
 
-    # fuzzy matching in company code-completion
-    company-flx
+      # fuzzy matching in company code-completion
+      company-flx
 
-    # project interaction library for Emacs
-    projectile
+      # project interaction library for Emacs
+      projectile
 
-    # Ivy completion for Emacs commands
-    ivy
-    ivy-hydra
+      # Ivy completion for Emacs commands
+      ivy
+      ivy-hydra
 
-    # extended search command
-    swiper
+      # extended search command
+      swiper
 
-    # Ivy completion support in projectile
-    counsel-projectile
+      # Ivy completion support in projectile
+      counsel-projectile
 
-    # Ivy interface to workspace symbols in LSP
-    lsp-ivy
+      # Ivy interface to workspace symbols in LSP
+      lsp-ivy
 
-    # Infer indentation offset from file contents
-    dtrt-indent
+      # Infer indentation offset from file contents
+      dtrt-indent
 
-    # draw line at suggested max line width
-    fill-column-indicator
+      # draw line at suggested max line width
+      fill-column-indicator
 
-    # show relative line numbers
-    linum-relative
+      # show relative line numbers
+      linum-relative
 
-    # rainbow colouring for brackets
-    rainbow-delimiters
+      # rainbow colouring for brackets
+      rainbow-delimiters
 
-    # Tree layout file explorer
-    treemacs
-    lsp-treemacs
+      # Tree layout file explorer
+      treemacs
+      lsp-treemacs
 
-    # show completion for multi-step key bindings
-    which-key
+      # show completion for multi-step key bindings
+      which-key
 
 
-    #####
-    # Language support
-    #####
+      #####
+      # Language support
+      #####
 
-    # haskell language support
-    haskell-mode
-    company-cabal
-    lsp-haskell
-    lsp-ui
-    yasnippet
+      # haskell language support
+      haskell-mode
+      company-cabal
+      lsp-haskell
+      lsp-ui
+      yasnippet
 
-    csv-mode
+      csv-mode
 
-    dockerfile-mode
-    docker-compose-mode
+      dockerfile-mode
+      docker-compose-mode
 
-    graphviz-dot-mode
+      graphviz-dot-mode
 
-    # JavaScript
-    tide
-    js2-mode
+      # JavaScript
+      tide
+      js2-mode
 
-    php-mode
+      php-mode
 
-    markdown-mode
+      markdown-mode
 
-    nginx-mode
+      nginx-mode
 
-    nix-mode
-    nix-sandbox
+      nix-mode
+      nix-sandbox
 
-    # git support
-    diff-hl
-    magit
+      # git support
+      diff-hl
+      magit
 
-    # ruby language support
-    robe
+      # ruby language support
+      robe
 
-    # HTML mode with support for embedded parts in other languages
-    # (for CSS, script tags, and templating engines in PHP, Python, etc)
-    web-mode
+      # HTML mode with support for embedded parts in other languages
+      # (for CSS, script tags, and templating engines in PHP, Python, etc)
+      web-mode
 
-    yaml-mode
-  ]);
+      yaml-mode
+    ]);
   };
 
   # launcher script for using emacs client
   home.packages = [(
     pkgs.writeShellScriptBin "ee" ''
-    if [[ $IN_NIX_SHELL = "" ]]; then
-      socketName="default"
-    else
-      socketName="nix-shell: $name"
-    fi
+      if [[ $IN_NIX_SHELL = "" ]]; then
+        socketName="default"
+      else
+        socketName="nix-shell: $name"
+      fi
 
-    emacsclient \
-      --socket-name "$socketName" \
-      --alternate-editor "" \
-      --create-frame \
-      --no-wait \
-      "$@"
+      emacsclient \
+        --socket-name "$socketName" \
+        --alternate-editor "" \
+        --create-frame \
+        --no-wait \
+        "$@"
     ''
   )];
 }
