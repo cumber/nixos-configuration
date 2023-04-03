9d1f012bffd7151c9b9db1d83feb04937f20201a
Benjamin Mellor <ben@smokingkangaroo.com>
2023-04-04 09:29:24 +1000

    flake.lock: Update
    Removed pipewire media-session config; has been replaced with
    WirePlumber. The issue I needed the configuration to fix seems not to
    be happening with WirePlumber, but I might need to add some config
    later to make my microphone work consistently.
    
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/b9e3a29864798d55ec1d6579ab97876bb1ee9664' (2023-03-02)
      → 'github:nix-community/home-manager/eefb37938639739251acd4bb68ecdaf7de2a13b5' (2023-04-03)
    • Updated input 'home-manager/utils':
        'github:numtide/flake-utils/5aed5285a952e0b949eb3ba02c12fa4fcfef535f' (2022-11-02)
      → 'github:numtide/flake-utils/93a2b84fc4b70d9e089d029deacc3583435c2ed6' (2023-03-15)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/a08d6979dd7c82c4cef0dcc6ac45ab16051c1169' (2023-03-01)
      → 'github:NixOS/nixpkgs/7c656856e9eb863c4d21c83e2601dd77f95f6941' (2023-04-02)

--- system/machines/kocka/default.nix
+++ system/machines/kocka/default.nix
@@ -20,29 +20,6 @@
         secretKeyFile = "/etc/nixos/secrets/nix-serve.sec";
       };
 
-      # By default pipewire assigns higher priority to the headphone
-      # playback port on the webcam than to my speakers. :(
-      pipewire = {
-        media-session.config.alsa-monitor = {
-          rules = [
-            {
-              actions = {
-                update-props = {
-                  "priority.driver" = 100;
-                  "priority.session" = 100;
-                };
-              };
-              matches = [
-                {
-                  "node.nick" = "WebMic HD Pro";
-                  "media.class" = "Audio/Sink";
-                }
-              ];
-            }
-          ];
-        };
-      };
-
       # Use proprietary nvidia driver
       xserver = {
         videoDrivers = [ "nvidia" ];
