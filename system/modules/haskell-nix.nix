36b9da2014a6fcc00af9a516e6a74030c82b0dbe
Benjamin Mellor <ben@smokingkangaroo.com>
2023-04-04 16:34:34 +1000

    add zw3rk binary cache for haskell.nix stuff
    This should be temporary (it's angerman's personal cache), but
    cache.iog.io has been missing a lot for quite a while now; using this
    makes things a lot less painful until that is sorted out.

--- system/modules/haskell-nix.nix
+++ system/modules/haskell-nix.nix
@@ -3,10 +3,12 @@
     nix.settings = {
       substituters = [
         "https://cache.iog.io"
+        "https://cache.zw3rk.com"
       ];
 
       trusted-public-keys = [
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
       ];
     };
   }
