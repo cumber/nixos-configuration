d2e723542a83ade4c2e2ac47de6d37fe30758a01
Benjamin Mellor <ben@smokingkangaroo.com>
2023-10-25 13:24:38 +1100

    update tutanota version

--- users/modules/tutanota/default.nix
+++ users/modules/tutanota/default.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }:
+let pname = "tutanota-desktop";
+    version = "3.118.13";
+    tutanota = pkgs.tutanota-desktop.overrideAttrs (old: {
+      src = pkgs.fetchurl {
+        url = "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-${version}/${pname}-${version}-unpacked-linux.tar.gz";
+        name = "tutanota-desktop-${version}.tar.gz";
+        hash = "sha256-3kpfF/XG7w6qUooS5UsntMKnggG1LhmV9f+R35kkmb0=";
+      };
+    });
+in
 {
-  home.packages = [ pkgs.tutanota-desktop ];
+  home.packages = [ tutanota ];
 }
