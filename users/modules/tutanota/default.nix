0847c8f408e1a59e50ae232295356e9323e0b787
Benjamin Mellor <ben@smokingkangaroo.com>
2023-11-09 15:34:21 +1100

    bump tutanota version override

--- users/modules/tutanota/default.nix
+++ users/modules/tutanota/default.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 let pname = "tutanota-desktop";
-    version = "3.118.13";
+    version = "3.118.25";
     tutanota = pkgs.tutanota-desktop.overrideAttrs (old: {
       src = pkgs.fetchurl {
         url = "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-${version}/${pname}-${version}-unpacked-linux.tar.gz";
         name = "tutanota-desktop-${version}.tar.gz";
-        hash = "sha256-3kpfF/XG7w6qUooS5UsntMKnggG1LhmV9f+R35kkmb0=";
+        hash = "sha256-iEb56vhL/gWrM8QPfkV8pD2D+BYv6Eb9MDisWfm2+L4=";
       };
     });
 in
