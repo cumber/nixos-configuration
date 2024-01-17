605f6fed12c14dd57d918bb985ee57a9923dde21
Benjamin Mellor <ben@smokingkangaroo.com>
2024-01-17 15:34:38 +1100

    flake.lock: Update
    Had to temporarily remove guitarix; re-enable when the build issue is fixed.
    
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/0e2e443ff24f9d75925e91b89d1da44b863734af' (2023-12-14)
      → 'github:nix-community/home-manager/16cefa78cc801911ebd4ff1faddc6280ab3c9228' (2024-01-16)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/a9bf124c46ef298113270b1f84a164865987a91c' (2023-12-11)
      → 'github:NixOS/nixpkgs/c3e128f3c0ecc1fb04aef9f72b3dcc2f6cecf370' (2024-01-15)

--- users/cumber/default.nix
+++ users/cumber/default.nix
@@ -66,7 +66,10 @@
 
     chatty
 
-    guitarix
+    # Guitarix build is broken; fix is already commited, working its
+    # way into the channel. I'm not really using it right now, so just
+    # disable for a bit.
+    #guitarix
 
     # LyX / LaTeX
     lyx
