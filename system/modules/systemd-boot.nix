70b81b5a5ba1b3db984ebdaa979edb1f931fa982
Benjamin Mellor <ben@smokingkangaroo.com>
2024-08-10 15:35:50 +1000

    enable plymouth boot screen for graphical luks password entry

--- system/modules/systemd-boot.nix
+++ system/modules/systemd-boot.nix
@@ -1,10 +1,21 @@
-{ config, ... }:
-  {
-    boot.loader = {
+{ ... }:
+{
+  boot = {
+    loader = {
       systemd-boot = {
         enable = true;
         editor = false;
       };
       efi.canTouchEfiVariables = true;
     };
-  }
+
+    plymouth.enable = true;
+
+    # plymouth can't show graphical luks password prompt without this
+    initrd.systemd.enable = true;
+
+    # plymouth otherwise waits for something else to time out before falling
+    # back to simpledrm
+    kernelParams = [ "plymouth.use-simpledrm" ];
+  };
+}
